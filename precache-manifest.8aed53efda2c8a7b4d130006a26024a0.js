319e20a7f590fbc21e96729d32b2a684f0dc2456
dpisati <43629737+dpisati@users.noreply.github.com>
2020-01-05 21:24:37 +1300

    Updates

---
+++ precache-manifest.8aed53efda2c8a7b4d130006a26024a0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c44374378922e5cd9279906cb812f68",
+    "url": "/weather_app/index.html"
+  },
+  {
+    "revision": "f57980d5698af5f0c8d2",
+    "url": "/weather_app/static/css/main.0cc48932.chunk.css"
+  },
+  {
+    "revision": "610f3ea873a433d699fd",
+    "url": "/weather_app/static/js/2.40044ae0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/weather_app/static/js/2.40044ae0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f57980d5698af5f0c8d2",
+    "url": "/weather_app/static/js/main.2556eb0a.chunk.js"
+  },
+  {
+    "revision": "d57628d6500e36fe238d",
+    "url": "/weather_app/static/js/runtime-main.ce6c808f.js"
+  },
+  {
+    "revision": "ded28d100572ae6922490d55f9466781",
+    "url": "/weather_app/static/media/background.ded28d10.jpg"
+  },
+  {
+    "revision": "58334f25e316ee591ef12d318443282d",
+    "url": "/weather_app/static/media/landscape_background.58334f25.jpg"
+  },
+  {
+    "revision": "e33c1c9dceb67b7bf9dd454313107059",
+    "url": "/weather_app/static/media/landscape_background_small.e33c1c9d.jpg"
+  }
+]);
